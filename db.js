f0598054a77bf24b7ef6cd0cb33910b9441ab932
Daniel <daniel@moberg.no>
2018-07-14 19:45:55 +0200

    pool

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+    connectionLimit : 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE
 });
 
-connection.connect();
+// connection.connect();
 
 module.exports = connection
