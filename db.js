6a1773c3030c8724eef111fd4f036422dc213067
Daniel <daniel@moberg.no>
2018-07-14 20:15:18 +0200

    connectionLimit: 100

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-    connectionLimit : 10,
+    connectionLimit : 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
